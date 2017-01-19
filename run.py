25a5804e30e5af6a2bb69c6081cee80ec12f2971
Shunji Makino <shunji.makino@gmail.com>
2017-01-19 12:47:25 +0900

    improve web view & time management & add feature of browser auto reflesh

--- run.py
+++ run.py
@@ -21,31 +21,38 @@ def hello():
         maxResults=5, singleEvents=True,
         orderBy='startTime').execute()
     events = eventsResult.get('items', [])
+    message = {}
     if not events:
-        return '予定がありません'
+        message['has_event'] = False
+        message['no_event_message'] = '予定がありません'
+        return render_template('index.html', message=message)
     for event in events:
         if 'dateTime' in event['start']:  # 時間指定イベントなら
             nearest_event = event
             break
     if nearest_event is None:
-        return '近いうちに時間指定の予定はありません'
+        message['has_event'] = False
+        message['no_event_message'] = '近いうちに時間指定の予定はありません'
+        return render_template('index.html', message=message)
 
     now = datetime.now(pytz.utc)
     start = nearest_event['start']['dateTime']
     start_datetime = datetime.replace(datetime.strptime(start, '%Y-%m-%dT%H:%M:%S+09:00'), tzinfo=timezone('Asia/Tokyo'))
     start = start_datetime.strftime('%Y年%m月%d日 %H:%M')
     remaining_time = start_datetime - now
-    message = {
-        'schedule': nearest_event['summary'],
-        'start': start
-    }
+    message['schedule'] = nearest_event['summary']
+    message['start'] = start
+    print('直近の予定'.format(message))
+    message['has_event'] = True
     alert_case = 0
     remaining_time_types = [5, 30, 60]
     if remaining_time.days < 1:
         for i, remaining_time_type in enumerate(remaining_time_types):
             if remaining_time.seconds < 60 * remaining_time_type:
-                alert_case = i + 1
                 message['remaining_time_type'] = remaining_time_type
+                message['remaining_time'] = int(remaining_time.seconds / 60)
+                if remaining_time_type * 60 - remaining_time.seconds < 80:
+                    alert_case = i + 1
                 break
     lego.alert(alert_case)
     message['alert_case'] = alert_case
